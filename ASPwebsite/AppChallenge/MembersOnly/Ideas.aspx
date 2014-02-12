8351bac261ffa0e29301cac4ed3d5a0164fb6ffb
U-Mavid-PC\Mavid <Mavid@Mavid-PC.(none)>
2014-02-11 20:08:20 -0800

    Allow users to submit ideas

--- ASPwebsite/AppChallenge/MembersOnly/Ideas.aspx
+++ ASPwebsite/AppChallenge/MembersOnly/Ideas.aspx
@@ -7,7 +7,21 @@
     <h2>
         Ideas
     </h2>
+      <asp:SqlDataSource
+          id="IdeaListDS"
+          runat="server"
+          DataSourceMode="DataReader"
+          ConnectionString="<%$ ConnectionStrings:Storage%>"
+          SelectCommand="SELECT ListEntry FROM IdeaList"> </asp:SqlDataSource>
+    <asp:TextBox ID="IdeaTextbox" runat="server" Height="106px"  Width="366px" TextMode="multiline"></asp:TextBox>
+    <br />
+
+    <asp:Button ID="SubmitIdeaButton" runat="server" onclick="SubmitIdeaButton_Click" Text="Button"  />
+    <asp:BulletedList ID="IdeaList" runat="server" DatasourceID = "IdeaListDS" DataTextField ="ListEntry" style="line-height:60px;" font-size="16">
+    </asp:BulletedList>
     <p>
-        Put content here.
-    </p>
-</asp:Content>
+        &nbsp;</p>
+      
+     
+ 
+    </asp:Content>
--- ASPwebsite/AppChallenge/MembersOnly/Ideas.aspx.cs
+++ ASPwebsite/AppChallenge/MembersOnly/Ideas.aspx.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Security;
 
 public partial class Ideas : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+      
+    }
+
+    protected void SubmitIdeaButton_Click(object sender, EventArgs e)
+    {
+        TextBox mIdeaTextbox = IdeaTextbox;
+        BulletedList mIdeaList = IdeaList;
+        String ideaText = mIdeaTextbox.Text;
+        String Username = HttpContext.Current.User.Identity.Name;
+        String Date = System.DateTime.Now.Date.ToShortDateString();
+
+        SqlConnection ideaListCon = new SqlConnection(ConfigurationManager.ConnectionStrings["storage"].ToString());
+        ideaListCon.Open();
+        string insertComText = "INSERT INTO IdeaList  (ListEntry, Author, Date) values('" + ideaText + " (submitted by " + Username + " on "+Date+")', '" + Username + "' , '" + Date + "')";
+        SqlCommand insertCom = new SqlCommand(insertComText, ideaListCon);
+        insertCom.BeginExecuteNonQuery();
+        mIdeaList.DataBind();
+        ideaListCon.Close();
+        
 
     }
 }
